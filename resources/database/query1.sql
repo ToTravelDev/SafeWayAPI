211d4da2abfdf0c83bc566dab8fc62ae85271f57
JOAO PEDRO SILVA DOS SANTOS <00294074@famacad.com.br>
2024-04-23 19:17:07 -0300

    Formulação do banco de dados

--- resources/database/query1.sql
+++ resources/database/query1.sql
@@ -1,24 +1,73 @@
--- Active: 1713397736041@@ep-cold-bush-a4pylmfr-pooler.us-east-1.aws.neon.tech@5432
-create table motorista(
-    mot_id serial PRIMARY KEY,
-    mot_nome character(70) not null,
-    mot_cpf character(14) not null unique,
-    mot_cidade character(30),
-    mot_uf character(2)
+/* banco de dados criado no SGBD PostgreSQL */
+
+create table tipo_usuario(
+    tp_id integer primary key,
+    tp_desc character varying(30) unique
 );
 
-/* relacionamento 1:1 com motorista */
-create Table motorista_endereco(
-    mot_end_id serial REFERENCES motorista(mot_id),
-    mot_end_cep CHARACTER(8),
-    mot_end_rua CHARACTER(50),
-    mot_end_numero CHARACTER(6),
-    mot_end_complemento CHARACTER(40)
+INSERT INTO tipo_usuario VALUES ('1', 'Aluno');
+INSERT INTO tipo_usuario VALUES ('2', 'Motorista');
+INSERT INTO tipo_usuario VALUES ('3', 'Escola');
+
+create table usuario (
+    usu_id serial primary key,
+    usu_nome character varying(100) not null,
+    usu_cpf character varying(14) not null unique,
+    usu_cidade character varying(30),
+    usu_uf character varying(2),
+    tp_usu integer,
+
+    CONSTRAINT tipo_usuario_fk FOREIGN KEY (tp_usu) REFERENCES tipo_usuario(tp_id)
 );
 
-create Table motorista_cnh(
-    mot_end_id serial REFERENCES motorista(mot_id),
-    mot_cnh_cat CHARACTER(2),
-    mot_cnh_numero CHARACTER(20),
-    mot_cnh_vencimento date
+create table usuario_login(
+    usu_log_email character varying(100) primary key,
+    usu_log_senha character varying(20) not null,
+    usu_log_sso boolean default(TRUE),
+    usu_id integer,
+
+    CONSTRAINT identificacao_usuario_login_fk FOREIGN KEY (usu_id) REFERENCES usuario(usu_id)
+);
+
+create table usuario_endereco(
+    usu_end_id serial primary key,
+    usu_id integer,
+    usu_end_rua character varying(100),
+    usu_end_numero character varying(6),
+    usu_end_complemento character varying(40),
+    usu_end_cep character varying(8),
+
+    CONSTRAINT identificacao_usuario_endereco_fk FOREIGN KEY (usu_id) references usuario(usu_id)
+);
+
+create table escola(
+    es_id serial primary key,
+    es_desc character varying(100),
+    es_ativo boolean default(TRUE),
+    es_cep character varying(8),
+    es_rua character varying(100),
+    es_numero character varying(6),
+    es_complemento character varying(50)
+);
+
+create table grupo_viagem(
+    vg_id serial primary key,
+    vg_ativo boolean default(TRUE),
+    vg_motorista_id integer,
+    vg_recorrente boolean default(FALSE),
+    vg_data_inicio date,
+    es_id integer,
+
+    CONSTRAINT motorista_viagem_id_fk FOREIGN KEY (vg_motorista_id) REFERENCES usuario(usu_id),
+    CONSTRAINT escola_viagem_id_fk FOREIGN KEY (es_id) REFERENCES escola(es_id)
+);
+
+create table grupo_viagem_integrantes(
+    vg_int_id serial primary key,
+    vg_id integer,
+    vg_int_usu_id integer,
+    vg_int_ativo boolean,
+
+    CONSTRAINT identificacao_viagem_fk FOREIGN KEY (vg_id) REFERENCES grupo_viagem(vg_id),
+    CONSTRAINT identificacao_aluno_fk FOREIGN KEY (vg_int_usu_id) REFERENCES usuario(usu_id)
 );
